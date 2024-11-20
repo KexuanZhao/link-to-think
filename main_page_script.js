27ede55ff9987238061a12e250c0d7f6582dbaf6
will7i1am3 <whaung.2003@gmail.com>
2024-11-20 16:14:26 -0500

    Fully functioning student main page

--- main_page_script.js
+++ main_page_script.js
@@ -6,6 +6,12 @@ let global_id = 0;
 let id_name = "research";
 
 let content = document.getElementsByClassName("content")[0];
+let searchInput = document.getElementById("search-input");
+let searchBtn = document.getElementById("search-button");
+let skillFilter = document.getElementById("skill-filter");
+let depFilter = document.getElementById("department-filter");
+let commitFilter = document.getElementById("commitment-filter");
+let compenFilter = document.getElementById("compensation-filter"); 
 
 function init() {
     // Initialization
@@ -20,12 +26,15 @@ function init() {
             research_info[index].professor,
             research_info[index].office,
             research_info[index].date,
+            research_info[index].compensation,
+            research_info[index].commitment,
             research_info[index].description
         );
         global_id++;
         content.innerHTML += str_html;
     }
     attachExpandListeners();
+    attachStarListeners();
 }
 
 function attachExpandListeners() {
@@ -42,25 +51,43 @@ function attachExpandListeners() {
             if (description.style.display === "none" || description.style.display === "") {
                 description.style.display = "block";
                 // Change icon to "remove"
-                const iconElement = event.target.closest("button").querySelector(".material-icons");
+                let iconElement = event.target.closest("button").querySelector(".material-icons");
                 iconElement.innerText = "remove";
             } else {
                 description.style.display = "none";
                 // Change icon to "remove"
-                const iconElement = event.target.closest("button").querySelector(".material-icons");
+                let iconElement = event.target.closest("button").querySelector(".material-icons");
                 iconElement.innerText = "add"; 
             }
         });
     });
 }
 
-function research_card_html(id, title, department, skills, prereq, professor, office, date, description) {
+function attachStarListeners() {
+    let starButtons = document.querySelectorAll(".star-btn");
+    starButtons.forEach((button)=> {
+        button.addEventListener("click", (event) => {
+            // Identify the corresponding description element
+            let card = event.target.closest(".research-card");
+            let star = card.querySelector(".star-icon");
+
+            // // Toggle the description visibility
+            if (star.style.color == "gold") {
+                star.style.color = "white";
+            } else {
+                star.style.color = "gold";
+            }
+        });
+    });
+}
+
+function research_card_html(id, title, department, skills, prereq, professor, office, date, compensation, commitment, description) {
     return `<div id="${id}" class="research-card">
                 <div class="research-header">
                     <div class="research-title">${title}</div>
                     <div class="research-actions">
                         <button class="apply-btn">Apply</button>
-                        <button class="icon-btn"><i class="material-icons star-icon">star</i></button>
+                        <button class="icon-btn star-btn"><i class="material-icons star-icon">star</i></button>
                     </div>
                 </div>
                 <div class="research-details">
@@ -71,6 +98,8 @@ function research_card_html(id, title, department, skills, prereq, professor, of
                         <button class="icon-btn email"><i class="material-icons mail-icon" style="font-size:1rem;">mail</i></button></span></div>
                     <div><span class="research-info">Office Location:</span> <span>${office}</span></div>
                     <div><span class="research-info">Date posted:</span> <span>${date}</span></div>
+                    <div><span class="research-info">Compensation:</span> <span>${compensation}</span></div>
+                    <div><span class="research-info">Commitment:</span> <span>${commitment}</span></div>
                 </div>                
                 <div id="${id}-description" class="research-description" style="display: none;">
                     ${description}
@@ -83,5 +112,64 @@ function research_card_html(id, title, department, skills, prereq, professor, of
             </div>`;
 }
 
+function filter() {
+    let skill = skillFilter.options[skillFilter.selectedIndex].text;
+    let department = depFilter.options[depFilter.selectedIndex].text;
+    let commitment = commitFilter.options[commitFilter.selectedIndex].text;
+    let compensation = compenFilter.options[compenFilter.selectedIndex].text;
+
+    global_id = 0; // reset global_id
+    id_name = "research";
+    content.innerHTML = ""; // reset content
+
+    let str_html;
+    for (let index in research_info) {
+        if ((skill !== "None") && (skill !== research_info[index].skills)) {
+            continue; // Skip this since it is not the case
+        }
+        
+        if ((department !== "None") && (department !== research_info[index].department)) {
+            continue; // Skip this since it is not the case
+        }
+
+        if ((commitment !== "None") && (commitment !== research_info[index].commitment)) {
+            continue; // Skip this since it is not the case
+        }
+
+        if ((compensation !== "None") && (compensation !== research_info[index].compensation)) {
+            continue; // Skip this since it is not the case
+        }
+
+        str_html = research_card_html(
+            id_name + global_id,
+            research_info[index].title,
+            research_info[index].department,
+            research_info[index].skills,
+            research_info[index].prereq,
+            research_info[index].professor,
+            research_info[index].office,
+            research_info[index].date,
+            research_info[index].compensation,
+            research_info[index].commitment,
+            research_info[index].description
+        );
+        global_id++;
+        content.innerHTML += str_html;
+    }
+    attachExpandListeners();
+    attachStarListeners();
+}
+
+// For now, it clears the input of the search
+function search() {
+    searchInput.value = "";
+}
+
 // Initialize the app
 init();
+searchBtn.addEventListener('click', search);
+skillFilter.addEventListener('change', filter);
+depFilter.addEventListener('change', filter);
+commitFilter.addEventListener('change', filter);
+compenFilter.addEventListener('change', filter);
+
